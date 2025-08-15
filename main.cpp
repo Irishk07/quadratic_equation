e46915df32101bb434ac679cd891f40ddcd79624
Irina Prygunova <prygunova.irina07@mail.ru>
2025-08-15 18:04:49 +0300

    оформила в виде структуры

--- main.cpp
+++ main.cpp
@@ -4,18 +4,23 @@
 const double EPS = 1e-9;
 
 void in_put(double *a, double *b, double *c);
-void solve_sq(double a, double b, double c, double *res);
-void solve_lin(double b, double c, double *res);
-void out_put(double *res);
+void solve_sq(double a, double b, double c, struct solve_equation *res_of_solving);
+void solve_lin(double b, double c, struct solve_equation *res_of_solving);
+void out_put(struct solve_equation *res_of_solving);
 int compare(double first_num, double second_num);
+
+struct solve_equation {
+    int count_roots;
+    double roots[2];
+};
 
 int main() 
 {
     double a = 0, b = 0, c = 0;
-    double res[3] = {0, 0, 0};
+    struct solve_equation res_of_solving = {0, {0, 0}};
     in_put(&a, &b, &c);
-    solve_sq(a, b, c, res);
-    out_put(res);
+    solve_sq(a, b, c, &res_of_solving);
+    out_put(&res_of_solving);
     return 0; 
 }
 
@@ -23,41 +28,41 @@ void in_put(double *a, double *b, double *c) {
     scanf("%lf %lf %lf", a, b, c); 
 }
 
-void solve_sq(double a, double b, double c, double *res) {
+void solve_sq(double a, double b, double c, struct solve_equation *res_of_solving) {
     if (!compare(a, 0)) {
         double discr = b * b - 4 * a * c;
         if (compare(discr, 0)) {
             double x = -b / (2 * a);
-            res[0] = 1;
-            res[1] = x;
+            res_of_solving -> count_roots = 1;
+            res_of_solving -> roots[0] = x;
         }
         else if (discr > 0) {
             double x1 = (-b + sqrt(discr)) / (2 * a);
             double x2 = (-b - sqrt(discr)) / (2 * a);
-            res[0] = 2;
-            res[1] = x1;
-            res[2] = x2;
+            res_of_solving -> count_roots = 2;
+            res_of_solving -> roots[0] = x1;
+            res_of_solving -> roots[1] = x2;
         }
         else {
-            res[0] = 0;
+            res_of_solving -> count_roots = 0;
         }
     }
     else {
-        solve_lin(b, c, res);
+        solve_lin(b, c, res_of_solving);
     }
 }
 
-void solve_lin(double b, double c, double *res) {
+void solve_lin(double b, double c, struct solve_equation *res_of_solving) {
     if (compare(b, 0) && !compare(c, 0)) {
-        res[0] = 0;
+        res_of_solving -> count_roots = 0;
     }
     else if (compare(b, 0) && compare(c, 0)) {
-        res[0] = 3;
+        res_of_solving -> count_roots = 3;
     }
     else {
         double x = -c / b;
-        res[0] = 1;
-        res[1] = x;
+        res_of_solving -> count_roots = 1;
+        res_of_solving -> roots[0] = x;
     }
 }
 
@@ -65,15 +70,15 @@ int compare(double first_num, double second_num) {
     return abs(first_num - second_num) < EPS ? 1 : 0;
 }
 
-void out_put(double *res) {
-    if (compare(res[0], 0)) {
+void out_put(struct solve_equation *res_of_solving) {
+    if (compare(res_of_solving -> count_roots, 0)) {
         printf("решений нет");
     }
-    else if (compare(res[0], 1)) {
-        printf("x = %lg", res[1]);
+    else if (compare(res_of_solving -> count_roots, 1)) {
+        printf("x = %lg", res_of_solving -> roots[0]);
     }
-    else if (compare(res[0], 2)) {
-        printf("x1 = %lg, x2 = %lg", res[1], res[2]);
+    else if (compare(res_of_solving -> count_roots, 2)) {
+        printf("x1 = %lg, x2 = %lg", res_of_solving -> roots[0], res_of_solving -> roots[1]);
     }
     else {
         printf("бесконечно много решений");
