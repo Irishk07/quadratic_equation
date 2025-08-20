dc861df81ad96819927f6a75e4a6934860d28159
Irina Prygunova <prygunova.irina07@mail.ru>
2025-08-20 20:47:40 +0300

    замена кодировки на 1251

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <locale.h>
 #include <stdio.h>
 #include <assert.h>
 #include "common.h"
@@ -8,6 +9,8 @@
 void welcome();
 
 int main() {
+    setlocale (LC_ALL, "");
+
     double all_coeffs [] = {0, 0, 0};
     solve_equation res_of_solving = {0, {0, 0}};
 
@@ -21,7 +24,7 @@ int main() {
     while (!check(all_coeffs));
 
     printf("Отлично! Приступим к решению :)\n");
-    printf("%s", jumping_cat);
+    print_cats("jumping_cat.txt");
 
     solve_sq(all_coeffs, &res_of_solving);
 
@@ -33,5 +36,4 @@ int main() {
 void welcome() {
     printf("Привет!\n");
     printf("Я твой помощник в решении квадратных уравнений\n");
-    printf("%s", sitting_cat);
 }
